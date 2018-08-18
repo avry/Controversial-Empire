ed3bf3e0884ac1173f0e0b7c50234b396f6a9789
Tan Avery <altan@ualberta.ca>
2018-08-18 00:10:44 -0600

    added team image

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,8 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={theteam} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={theteam} className="App-team-picture" />
         </header>
         <p className="App-intro">
           This is Controversial Empire, a homie g Dawg production work  
