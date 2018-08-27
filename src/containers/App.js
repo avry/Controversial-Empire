2ca4600b7a98ab9ce747c87977cfdfd312082622
Tan Avery <altan@ualberta.ca>
2018-08-27 02:00:52 -0600

    GET method implemented. Must implement csrf tokens to get PUT method implemented

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import DataProvider from "./DataProvider";
 import Table from "./Table";
+import Form from "./Form";
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <DataProvider endpoint="api/lead/" render={data => <Table data={data} />} />
+      	<Form endpoint="api/lead/" />
       </div>
     );
   }
