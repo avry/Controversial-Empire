a49351f5d21dec927c02fb6696384e6d113f57f5
Tan Avery <altan@ualberta.ca>
2018-09-16 00:39:56 -0600

    purple column

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,14 +10,12 @@ const Home = () => (
   <div>
   	<Container fluid>
 	  	<Row>
-	  		<div className="col-lg-5 offset-lg-4">
-		  		<div className="home-page">
-		  			<img src={SillyHat} alt="Profile Pict" style={{ height: "300px", width:"200px" , paddingTop: "20px", paddingRight:"10px" , paddingBottom: "10px", float:"left" }} />
-		  			<p style={{ marginTop: "20px" }}> </p>
-		  			<p>A responsible individual with an ability to solve hard and complex problems.
-		  			Understands that behind every complex system is a pattern or method to it.
-		  			Recent graduate from the University of Alberta with an Honors Degree in Computer Science.</p>
-		  		</div>
+	  		<div className="col-lg-5 offset-lg-4 home-page">
+	  			<img src={SillyHat} alt="Profile Pict" style={{ height: "300px", width:"200px" , paddingTop: "20px", paddingRight:"10px" , paddingBottom: "10px", float:"left" }} />
+	  			<p style={{ marginTop: "20px" }}> </p>
+	  			<p>A responsible individual with an ability to solve hard and complex problems.
+	  			Understands that behind every complex system is a pattern or method to it.
+	  			Recent graduate from the University of Alberta with an Honors Degree in Computer Science.</p>
 	  		</div>
 	  	</Row>	
     </Container>
