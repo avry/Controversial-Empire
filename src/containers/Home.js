f850c5015b0c5c43619aa946e118017a47519069
Tan Avery <altan@ualberta.ca>
2018-09-16 00:33:36 -0600

    played around with Home page. Experimenting with blocks of colour

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+
 import SillyHat from '../images/SillyHat.jpg';
+import './Home.css';
+
 
 
 const Home = () => (
   <div>
   	<Container fluid>
 	  	<Row>
-	  		<div className="col-md-4 offset-md-3">
-	  			<img src={SillyHat} alt="Profile Pict" style={{ height: "300px", width:"200px" , paddingTop: "20px" , paddingBottom: "10px" }} />
-	  			<p>A responsible individual with an ability to solve hard and complex problems.
-	  			Understands that behind every complex system is a pattern or method to it.
-	  			Recent graduate from the University of Alberta with an Honors Degree in Computer Science.</p>
+	  		<div className="col-lg-5 offset-lg-4">
+		  		<div className="home-page">
+		  			<img src={SillyHat} alt="Profile Pict" style={{ height: "300px", width:"200px" , paddingTop: "20px", paddingRight:"10px" , paddingBottom: "10px", float:"left" }} />
+		  			<p style={{ marginTop: "20px" }}> </p>
+		  			<p>A responsible individual with an ability to solve hard and complex problems.
+		  			Understands that behind every complex system is a pattern or method to it.
+		  			Recent graduate from the University of Alberta with an Honors Degree in Computer Science.</p>
+		  		</div>
 	  		</div>
 	  	</Row>	
     </Container>
