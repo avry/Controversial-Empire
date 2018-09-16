87286e8e7a7d56963167e4779819b98e41e28a8d
Tan Avery <altan@ualberta.ca>
2018-09-15 20:45:34 -0600

    fixed home screen

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,17 +7,13 @@ const Home = () => (
   <div>
   	<Container fluid>
 	  	<Row>
-	  		<Col></Col>
-	  		<Col>
+	  		<div className="col-md-4 offset-md-3">
 	  			<img src={SillyHat} alt="Profile Pict" style={{ height: "300px", width:"200px" , paddingTop: "20px" , paddingBottom: "10px" }} />
 	  			<p>A responsible individual with an ability to solve hard and complex problems.
 	  			Understands that behind every complex system is a pattern or method to it.
 	  			Recent graduate from the University of Alberta with an Honors Degree in Computer Science.</p>
-	  		</Col>
-	  		<Col>
-	  		</Col>
-	  	</Row>
-    	
+	  		</div>
+	  	</Row>	
     </Container>
   </div>
 )
