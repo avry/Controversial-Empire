623d9dc6f11829a2969a47233caf918b05b86805
Tan Avery <altan@ualberta.ca>
2018-10-02 19:06:30 -0600

    renamed containers to components and removed photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,11 +11,12 @@ const Home = () => (
   	<Container fluid>
 	  	<Row>
 	  		<div className="home-page">
-	  			<img src={SillyHat} alt="Profile Pict" style={{ height: "300px", width:"200px" , paddingLeft:"20px" , paddingTop: "20px", paddingRight:"10px" , paddingBottom: "10px", float:"left" }} />
+	  			<img src={} alt="Profile Pict" style={{ height: "300px", width:"200px" , paddingLeft:"20px" , paddingTop: "20px", paddingRight:"10px" , paddingBottom: "10px", float:"left" }} />
 	  			<p style={{ marginTop: "20px" }}> </p>
 	  			<h3>I have a word</h3>
 	  			<p>A responsible individual with an ability to solve hard problems and make things work.
 	  			Understands that behind every complex system is a pattern or method to it.
+	  			Serious about science and using data to arrive at reasonably confident underlying relationships between things.
 	  			Is constantly learning and fiddling with new technologies and frameworks.
 	  			Recent graduate from the University of Alberta with an Honors Degree in Computer Science.</p>
 	  		</div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './containers/App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
