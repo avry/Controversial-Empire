92764e1f40cc7029d1a29db254d9d6f568c8bf0d
Tan Avery <altan@ualberta.ca>
2018-10-02 19:26:41 -0600

    removed photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,14 @@ import SillyHat from '../images/SillyHat.jpg';
 import './Home.css';
 
 
+//<img src={''} alt="Profile Pict" style={{ height: "300px", width:"200px" , paddingLeft:"20px" , paddingTop: "20px", paddingRight:"10px" , paddingBottom: "10px", float:"left" }} />
+
 
 const Home = () => (
   <div>
   	<Container fluid>
 	  	<Row>
 	  		<div className="home-page">
-	  			<img src={} alt="Profile Pict" style={{ height: "300px", width:"200px" , paddingLeft:"20px" , paddingTop: "20px", paddingRight:"10px" , paddingBottom: "10px", float:"left" }} />
 	  			<p style={{ marginTop: "20px" }}> </p>
 	  			<h3>I have a word</h3>
 	  			<p>A responsible individual with an ability to solve hard problems and make things work.
